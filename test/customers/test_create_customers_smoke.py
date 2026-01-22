59ac500e8d3079d4386fa34b94ab50e07ac62da2
Viktor Kuznietsov <desaga@gmail.com>
2026-01-22 14:35:56 -0600

    added marks

--- test/customers/test_create_customers_smoke.py
+++ test/customers/test_create_customers_smoke.py
@@ -1,4 +1,7 @@
 import logging
+
+import pytest
+
 from src.generators.credentials import get_random_password_and_email
 from src.clients.customers_client import CustomersClient
 from src.payloads.customers_payloads import build_create_customer_payload
@@ -9,6 +12,9 @@ from src.db.dal.users_dal import get_existed_customer_email
 import pdb
 
 logger = logging.getLogger(__name__)
+
+
+@pytest.mark.tc01
 def test_create_customer_only_email_password():
     logger.info('-> test_create_customer_only_email_password')
     # get test data
@@ -23,8 +29,11 @@ def test_create_customer_only_email_password():
     # verify email in response
     # verify user name in response
     del test_data['password']
-    assert_customer_created(response, expected_status=201, expected_body=test_data)
+    assert_customer_created(response, expected_status=201,
+                            expected_body=test_data)
 
+@pytest.mark.skip(reason="Feature not implemented yet")
+@pytest.mark.tc02
 def test_customer_has_created_in_database():
     logger.info('-> test_customer_has_created_in_database')
     test_data = get_random_password_and_email()
@@ -33,6 +42,8 @@ def test_customer_has_created_in_database():
     response = c_client.create_customer(payload)
     assert_customer_exists_in_db(test_data['email'])
 
+
+@pytest.mark.tc03
 def test_create_customer_with_existed_email():
     logger.info('-> test_create_customer_with_existed_email')
     test_data = get_random_password_and_email()
@@ -41,5 +52,3 @@ def test_create_customer_with_existed_email():
     c_client = CustomersClient()
     response = c_client.create_customer(payload)
     assert_customer_with_existed_email_not_created(response, 400)
-
-
